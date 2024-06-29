8f3ea839d9d2554148fb7fa7c2f10f108e8e1c44
Sebastian Murgul <DerWaldi@users.noreply.github.com>
2024-06-29 19:50:06 +0200

    fix: linting errors

---
+++ Tiny-I2C-Drivers.h
@@ -0,0 +1,15 @@
+/* TinyOLED v0.0.1
+
+   Sebastian Murgul - www.sebastian-murgul.com - 29th June 2022
+
+   CC BY 4.0
+   Licensed under a Creative Commons Attribution 4.0 International license:
+   http://creativecommons.org/licenses/by/4.0/
+*/
+
+#ifndef TinyI2CDrivers_h
+#define TinyI2CDrivers_h
+
+#include "TinyOLED.h"
+
+#endif
--- examples/ssd1306_oled_text_128x32/ssd1306_oled_text_128x32.ino
+++ examples/ssd1306_oled_text_128x32/ssd1306_oled_text_128x32.ino
@@ -1,4 +1,4 @@
-#include <TinyOLED.h>
+#include <Tiny-I2C-Drivers.h>
 
 // messages to print on OLED
 const char Message1[] = "HELLO WORLD !";
@@ -9,37 +9,40 @@ const char Message5[] = "DOG  - (0123456789)";
 
 TinyOLED oled;
 
-void setup() {
-  oled.init();                            // initialize the OLED
-  oled.clear();                           // clear screen
+void setup()
+{
+  oled.init();  // initialize the OLED
+  oled.clear(); // clear screen
 }
 
-void loop() {
+void loop()
+{
   // print messages
-  oled.clear();                           // clear screen
-  oled.cursor(20, 0);                     // set cursor position
-  oled.printStr(Message1);                // print message 1
-  delay(1000);                            // wait a second
-  oled.cursor(0, 2);                      // set cursor position
-  oled.printStr(Message2);                // print message 2
-  delay(4000);                            // wait 4 seconds
+  oled.clear();            // clear screen
+  oled.cursor(20, 0);      // set cursor position
+  oled.printStr(Message1); // print message 1
+  delay(1000);             // wait a second
+  oled.cursor(0, 2);       // set cursor position
+  oled.printStr(Message2); // print message 2
+  delay(4000);             // wait 4 seconds
   oled.clear();
-  oled.printStr(Message3);                // print message 3
-  oled.cursor(0, 1);                      // set cursor next line
-  oled.printStr(Message4);                // print message 4
-  oled.cursor(0, 2);                      // set cursor next line
-  oled.printStr(Message5);                // print message 5
-  delay(4000);                            // wait 4 seconds
+  oled.printStr(Message3); // print message 3
+  oled.cursor(0, 1);       // set cursor next line
+  oled.printStr(Message4); // print message 4
+  oled.cursor(0, 2);       // set cursor next line
+  oled.printStr(Message5); // print message 5
+  delay(4000);             // wait 4 seconds
 
   // print all characters
-  oled.cursor(0, 0);                      // set cursor at upper left corner
-  for(uint8_t i=32; i<64+32; i++) 
-    oled.printChar(i);                    // print all characters
-  delay(3000);                            // wait 3 seconds
+  oled.cursor(0, 0); // set cursor at upper left corner
+  for (uint8_t i = 32; i < 64 + 32; i++)
+    oled.printChar(i); // print all characters
+  delay(3000);         // wait 3 seconds
 
   // scroll out the text
-  for (uint8_t i=0; i<32; i++) {          // shift 32 pixels upwards
-    oled.shift(i);                        // set vertical shift value
-    delay(100);                           // delay a bit
+  for (uint8_t i = 0; i < 32; i++)
+  {                // shift 32 pixels upwards
+    oled.shift(i); // set vertical shift value
+    delay(100);    // delay a bit
   }
 }
